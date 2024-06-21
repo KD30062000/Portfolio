065015967f2bda644a4798c4a189a2b179a963f1
KD30062000 <159777650+KD30062000@users.noreply.github.com>
2024-06-21 22:42:25 +0530

    allow all hosts
    update requirements

--- portfolioProject/settings.py
+++ portfolioProject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure--x#-j#zl33t1*6$0y*^^ggyecmg=9=q&^5bcqeerpqhqr6+!)*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
